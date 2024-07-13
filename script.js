3281d99e0300ad3f6e0dafb0ba9b38c2b6f94604
Sapana Dashoni <140044144+SapanaDashoni15@users.noreply.github.com>
2024-07-14 00:18:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 fetch('https://icanhazdadjoke.com/slack')
-    .then(response => response.json()) // Corrected 'data' to 'response'
+    .then(response => response.json()) 
     .then(jokeData => {
         const jokeText = jokeData.attachments[0].text;
-        const jokeElement = document.getElementById('jokeElement'); // Corrected 'decument' to 'document'
+        const jokeElement = document.getElementById('jokeElement'); /
 
         jokeElement.innerHTML = jokeText;
     })
     .catch(error => {
         console.error('Error fetching joke:', error);
-        // Handle error appropriately, e.g., show an error message in the UI
+      
     });
